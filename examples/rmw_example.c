267f1ad2f87906cf5ea57fa263cf73bcb029c0ae
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2024-06-23 17:25:10 +0800

    Worker do the job to execute BBP
    1. Add the tpool_future variable
    - tpool_future to pass the result to the main thread.
    - The mutex lock and the condition variable to ensure concurrency.
    
    2. The main thread sequentially accumulate results from BBP that
    calcuate by every worker.
    - Wait using `tpool_future_get()` until the condition variable is
    broadcast to confirm that the result has been marked as
    __FUTURE_FINISHED.
    
    3. Change `thread_pool` to `tpool` to improve readability.
    
    4. Add the Makefile.

--- examples/rmw_example.c
+++ examples/rmw_example.c
@@ -4,18 +4,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+
 #include <math.h>
-#include <stdio.h>
+#include <stddef.h>
 
 #define PRECISION 100 /* upper bound in BPP sum */
 
 #define CACHE_LINE_SIZE 64
-#define N_JOBS 16
-#define N_THREADS 8
+#define N_THREADS 64
 
+struct tpool_future {
+    atomic_int flag;
+    void *result;
+    mtx_t mutex;
+    cnd_t cond_finished;
+};
 
 typedef struct job {
+    void *(*func)(void *);
     void *args;
+    struct tpool_future *future;
     struct job *next, *prev;
 } job_t;
 
@@ -25,9 +33,9 @@ typedef struct idle_job {
     job_t job;
 } idle_job_t;
 
-enum state { idle, running, cancelled };
+enum state { __FUTURE_START, __FUTURE_FINISHED, idle, running, cancelled };
 
-typedef struct thread_pool {
+typedef struct tpool {
     atomic_flag initialezed;
     int size;
     thrd_t *pool;
@@ -35,13 +43,50 @@ typedef struct thread_pool {
     thrd_start_t func;
     // job queue is a SPMC ring buffer
     idle_job_t *head;
-} thread_pool_t;
+} tpool_t;
+
+static struct tpool_future *tpool_future_create(void)
+{
+    struct tpool_future *future = malloc(sizeof(struct tpool_future));
+    if (future) {
+        future->flag = __FUTURE_START;
+        future->result = NULL;
+        mtx_init(&future->mutex, mtx_plain);
+        cnd_init(&future->cond_finished);
+    }
+    return future;
+}
+
+void tpool_future_get(struct tpool_future *future)
+{
+    mtx_lock(&future->mutex);
+    while ((future->flag & __FUTURE_FINISHED) == 0) {
+        cnd_wait(&future->cond_finished, &future->mutex);
+    }
+    mtx_unlock(&future->mutex);
+}
+
+int tpool_future_destroy(struct tpool_future *future)
+{
+    if (future) {
+        mtx_lock(&future->mutex);
+        if (future->flag & __FUTURE_FINISHED) {
+            mtx_unlock(&future->mutex);
+            mtx_destroy(&future->mutex);
+            cnd_destroy(&future->cond_finished);
+            free(future);
+        } else {
+            mtx_unlock(&future->mutex);
+        }
+    }
+    return 0;
+}
 
 static int worker(void *args)
 {
     if (!args)
         return EXIT_FAILURE;
-    thread_pool_t *thrd_pool = (thread_pool_t *)args;
+    tpool_t *thrd_pool = (tpool_t *)args;
 
     while (1) {
         if (atomic_load(&thrd_pool->state) == cancelled)
@@ -50,14 +95,22 @@ static int worker(void *args)
             // claim the job
             job_t *job = atomic_load(&thrd_pool->head->prev);
             while (!atomic_compare_exchange_weak(&thrd_pool->head->prev, &job,
-                                                   job->prev)) {
+                                                 job->prev)) {
             }
             if (job->args == NULL) {
                 atomic_store(&thrd_pool->state, idle);
             } else {
-                printf("Hello from job %d\n", *(int *)job->args);
+                void *ret_value = job->func(job->args);
+
+                mtx_lock(&job->future->mutex);
+
+                job->future->flag |= __FUTURE_FINISHED;
+                job->future->result = ret_value;
+                cnd_broadcast(&job->future->cond_finished);
+
+                mtx_unlock(&job->future->mutex);
                 free(job->args);
-                free(job); // could cause dangling pointer in other threads
+                free(job);
             }
         } else {
             /* To auto run when jobs added, set status to running if job queue is not empty.
@@ -66,9 +119,10 @@ static int worker(void *args)
             continue;
         }
     };
+    return EXIT_SUCCESS;
 }
 
-static bool thread_pool_init(thread_pool_t *thrd_pool, size_t size)
+static bool tpool_init(tpool_t *thrd_pool, size_t size)
 {
     if (atomic_flag_test_and_set(&thrd_pool->initialezed)) {
         printf("This thread pool has already been initialized.\n");
@@ -106,7 +160,7 @@ static bool thread_pool_init(thread_pool_t *thrd_pool, size_t size)
     return true;
 }
 
-static void thread_pool_destroy(thread_pool_t *thrd_pool)
+static void tpool_destroy(tpool_t *thrd_pool)
 {
     if (atomic_exchange(&thrd_pool->state, cancelled))
         printf("Thread pool cancelled with jobs still running.\n");
@@ -124,16 +178,34 @@ static void thread_pool_destroy(thread_pool_t *thrd_pool)
     atomic_flag_clear(&thrd_pool->initialezed);
 }
 
-__attribute__((nonnull(2))) static bool add_job(thread_pool_t *thrd_pool,
-                                                void *args)
+/* Use Bailey–Borwein–Plouffe formula to approximate PI */
+static void *bbp(void *arg)
+{
+    int k = *(int *)arg;
+    double sum = (4.0 / (8 * k + 1)) - (2.0 / (8 * k + 4)) -
+                 (1.0 / (8 * k + 5)) - (1.0 / (8 * k + 6));
+    double *product = malloc(sizeof(double));
+    if (product)
+        *product = 1 / pow(16, k) * sum;
+    return (void *)product;
+}
+
+struct tpool_future *add_job(tpool_t *thrd_pool, void *(*func)(void *),
+                             void *args)
 {
     // May use memory pool for jobs
     job_t *job = malloc(sizeof(job_t));
     if (!job)
-        return false;
+        return NULL;
+
+    struct tpool_future *future = tpool_future_create();
+    if (!future)
+        return NULL;
 
     // unprotected producer
     job->args = args;
+    job->func = bbp;
+    job->future = future;
     job->next = thrd_pool->head->job.next;
     job->prev = &thrd_pool->head->job;
     thrd_pool->head->job.next->prev = job;
@@ -143,50 +215,47 @@ __attribute__((nonnull(2))) static bool add_job(thread_pool_t *thrd_pool,
         // trap worker at idle job
         thrd_pool->head->job.prev = &thrd_pool->head->job;
     }
-
-    return true;
+    return future;
 }
 
-static inline void wait_until(thread_pool_t *thrd_pool, int state)
+static inline void wait_until(tpool_t *thrd_pool, int state)
 {
     while (atomic_load(&thrd_pool->state) != state) {
         thrd_yield();
     }
 }
 
-/* Use Bailey–Borwein–Plouffe formula to approximate PI */
-static void *bbp(void *arg)
-{
-    int k = *(int *) arg;
-    double sum = (4.0 / (8 * k + 1)) - (2.0 / (8 * k + 4)) -
-                 (1.0 / (8 * k + 5)) - (1.0 / (8 * k + 6));
-    double *product = malloc(sizeof(double));
-    if (product)
-        *product = 1 / pow(16, k) * sum;
-    return (void *) product;
-}
-
 int main()
 {
-    thread_pool_t thrd_pool = { .initialezed = ATOMIC_FLAG_INIT };
-    if (!thread_pool_init(&thrd_pool, N_THREADS)) {
+    struct tpool_future *futures[PRECISION + 1];
+    double bbp_sum = 0;
+
+    tpool_t thrd_pool = { .initialezed = ATOMIC_FLAG_INIT };
+    if (!tpool_init(&thrd_pool, N_THREADS)) {
         printf("failed to init.\n");
         return 0;
     }
-    for (int i = 0; i < N_JOBS; i++) {
+    for (int i = 0; i <= PRECISION; i++) {
         int *id = malloc(sizeof(int));
         *id = i;
-        add_job(&thrd_pool, id);
+        futures[i] = add_job(&thrd_pool, bbp, id);
     }
     // Due to simplified job queue (not protecting producer), starting the pool manually
     atomic_store(&thrd_pool.state, running);
     wait_until(&thrd_pool, idle);
-    for (int i = 0; i < N_JOBS; i++) {
+    for (int i = 0; i <= PRECISION; i++) {
         int *id = malloc(sizeof(int));
         *id = i;
-        add_job(&thrd_pool, id);
+        add_job(&thrd_pool, bbp, id);
+    }
+
+    for (int i = 0; i <= PRECISION; i++) {
+        tpool_future_get(futures[i]);
+        bbp_sum += *(double *)(futures[i]->result);
+        tpool_future_destroy(futures[i]);
     }
     atomic_store(&thrd_pool.state, running);
-    thread_pool_destroy(&thrd_pool);
+    tpool_destroy(&thrd_pool);
+    printf("PI calculated with %d terms: %.15f\n", PRECISION + 1, bbp_sum);
     return 0;
 }
